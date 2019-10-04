e464ed2fcea4598904ed60f4e7b4f501c97ac020
IvanovVlad <50176565+IvanovVlad@users.noreply.github.com>
2019-10-04 19:27:58 +0400

    Solved

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let decoded = '';
+    for (let i = 0; i < expr.length; i += 10) {
+
+        let BinaryCode = expr.substr(i, 10);
+
+        if (BinaryCode == '**********') {
+            decoded += ' ';
+        } else {
+
+            let MorseCode = '';
+
+            for (let j = 0; j < expr.length; j += 2) {
+
+                let symbol = BinaryCode.substr(j, 2);
+
+                if (symbol == '10') {
+                    MorseCode += '.';
+                }else if (symbol == '11') {
+                    MorseCode += '-';
+                }
+            }
+            decoded += MORSE_TABLE[MorseCode];
+        }
+    }
+
+    return decoded;
 }
 
 module.exports = {
